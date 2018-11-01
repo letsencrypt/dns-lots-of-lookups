46d6f8e21e415cde03fd6a424c54f4cbaf557a8c
Daniel <cpu@letsencrypt.org>
2018-11-01 13:16:41 -0400

    Add simple indexes on results table.

--- db-schema.sql
+++ db-schema.sql
@@ -26,6 +26,10 @@ CREATE TABLE `results` (
 	`serverID` INT NOT NULL,
 	`experimentID` INT NOT NULL,
 	PRIMARY KEY (`id`),
+	KEY `results_name_idx` (`name`),
+	KEY `results_type_idx` (`type`),
+	KEY `results_error_idx` (`error`(20)),
 	CONSTRAINT `results_serverID_servers` FOREIGN KEY (`serverID`) REFERENCES servers (`id`),
 	CONSTRAINT `results_experimentID_experiments` FOREIGN KEY (`experimentID`) REFERENCES experiments (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
